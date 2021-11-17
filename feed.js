dc1d9ea3f5d77711671b49944e0581d1581e4e30
Kristen Rupinen <kristenrupinen@Kristens-MacBook-Pro-2.local>
2021-11-17 15:22:34 -0600

    change

--- feed.js
+++ feed.js
@@ -25,7 +25,6 @@ function setFeed(json)
 
 
     json.forEach(vendor => {
-    html += "<li>"+vendor.merchType+"</li>";
     html += `<div class = "post">`;
     html += `<h2>${vendor.venName}</h2>`;
     html += `<img src="${vendor.images}" alt = "https://multichannelmerchant.com/wp-content/uploads/2019/09/stupid-vendor-image-with-scrabble-tiles-feature.jpg" width = "auto" height = "auto"></img>`;
