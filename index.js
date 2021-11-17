647d8514e58dc4b33a5d854ac5a56eb4e1cd05fd
Kristen Rupinen <kristenrupinen@Kristens-MacBook-Pro-2.local>
2021-11-17 16:19:03 -0600

    trying login

---
+++ index.js
@@ -0,0 +1,26 @@
+function handleOnSubmit()
+{
+    const feedUrl = "https://big-als-farmers-market-backend.herokuapp.com/api/Vendor";
+    fetch(peopleUrl).then(function(response){
+        return response.json();
+    }).then(function(json){
+        json.forEach(vendor =>{
+            if(vendor.id = document.getElementById("idBox"))
+            {
+                if(vendor.username = document.getElementById("userBox"))
+                {
+                    if(vendor.password=document.getElementById("passBox"))
+                    {
+                        href = "practice.html";
+                
+                    }
+                }
+            }
+        })
+        
+
+    }).catch(function(error){
+        console.log(error);
+    });
+}
+
