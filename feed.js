a6937efced5431d1353d270fadf69689b690aeb2
Kristen Rupinen <kristenrupinen@Kristens-MacBook-Pro-2.local>
2021-11-17 15:46:44 -0600

    html moved

--- feed.js
+++ feed.js
@@ -27,7 +27,7 @@ function setFeed(json)
     json.forEach(vendor => {
     html += `<div class = "post">`;
     html += `<h2>${vendor.vendorName}</h2>`;
-    html += `<img src="${vendor.image}" alt img src = "https://m.media-amazon.com/images/I/71fTy0gwcrL._AC_SX425_.jpg" width = "500" height = "300"></img>`;
+    html += `<img src="${vendor.image}" width = "500" height = "300" onerror="this.src='https://m.media-amazon.com/images/I/71fTy0gwcrL._AC_SX425_.jpg'"></img>`;
     html += `<h3>Merch Type: ${vendor.merchType}</h3>`
     html += `<p>${vendor.description}</p>`;
     html += `</div>`;
