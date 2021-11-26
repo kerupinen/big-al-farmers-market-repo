f4242498a1b6d196529a2d001f140adc3f072c2f
Kristen Rupinen <kristenrupinen@Kristens-MacBook-Pro-2.local>
2021-11-26 18:06:33 -0500

    attendee registration

--- resources/scripts/attendeeregister.js
+++ resources/scripts/attendeeregister.js
@@ -1,1 +1,33 @@
 function handleOnLoad(){}
+
+function registerAttendee()
+{
+    const feedUrl = "https://big-als-farmers-market-backend.herokuapp.com/api/Attendee/attendeeRegister";
+    fetch(feedUrl, {
+        method: "POST",
+        headers: 
+        {
+            "Accept": "application/json",
+            "Content-type": "application/json"
+        },
+
+        body: JSON.stringify({
+            registrationNum : sessionStorage.getItem("loggedInAttendee")
+        })
+    }).then(response => response.json())
+    .then(function(json){
+        if(json.registrationNum!=0)
+        {
+            window.alert("You have been registered! Your Number is " +json.registrationNum);
+        }
+        else{
+            window.alert("You were already signed up! Your Number is "+json.registrationNum);
+        }
+        
+        window.location.href = "feed.html";
+        console.log(json);
+    }).catch(function(error){
+        console.log(error);
+    });
+
+}
