fdef947591ea3c60b60716e430437600f9e4b24c
Kristen Rupinen <kristenrupinen@Kristens-MacBook-Pro-2.local>
2021-11-27 13:19:50 -0500

    reports

--- resources/scripts/reportsPage.js
+++ resources/scripts/reportsPage.js
@@ -1,13 +1,24 @@
 function handleOnLoad()
 {
     //getting url
-    const feedUrl = "https://big-als-farmers-market-backend.herokuapp.com/api/Vendor";
+   const vendorsUrl = "https://big-als-farmers-market-backend.herokuapp.com/api/Vendor";
     //getting posts from url
-    fetch(feedUrl).then(response => response.json())
+    fetch(vendorsUrl).then(response => response.json())
     .then(function(vendors){
         console.log(vendors);
         //showing posts
         displayTable(vendors);
+    }).catch(function(error){
+        console.log(error);
+    });
+    
+
+    const attendeesUrl = "https://big-als-farmers-market-backend.herokuapp.com/api/Attendee";
+    fetch(attendeesUrl).then(response => response.json())
+    .then(function(attendees){
+        console.log(attendees);
+        //showing posts
+        displayAttendee(attendees);
     }).catch(function(error){
         console.log(error);
     });
@@ -16,12 +27,25 @@ function handleOnLoad()
 
 function displayTable(vendors)
 {
-     //var dataTable = document.getElementById("dataTable");
+          var dataTable = document.getElementById("vendorTable");
           var html = "<table><tr><th>Vendor Id</th><th>Vendor Registered Spot</th><th>Vendor name</th><th>Merchandise Type</th></tr>";
-          vendors.forEach(vendor => {
-           html+=`<tr><td>${vendors.venNum}</td><td>${vendors.RegisterSpot}</td><td>${vendors.venName}</td><td>${vendors.merchType}</td></tr>`;
+          vendors.forEach(vendors => {
+           html+=`<tr><td>${vendors.venNum} </td><td>${vendors.registerSpot}</td><td>${vendors.vendorName}</td><td>${vendors.merchType}</td></tr>`;
       
           });
           html+="</table>";
           dataTable.innerHTML = html;
+}
+
+function displayAttendee(attendees)
+{
+          var newTable = document.getElementById("attendeeTable");
+          var htmls = "<table><tr><th>Attendee Id</th><th>Attendee Registered Spot</th><th>Attendee Last Name</th><th>Attendee First Name</th></tr>";
+          attendees.forEach(attendees => {
+           htmls+=`<tr><td>${attendees.attendeeNum} </td><td>${attendees.registrationNum}</td><td>${attendees.lastName}</td><td>${attendees.firstName}</td></tr>`;
+      
+          });
+          htmls+="</table>";
+          newTable.innerHTML = htmls;
+
 }
