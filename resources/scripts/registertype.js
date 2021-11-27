a6bc776dcb93a99ed4f5b095cc6fd0deea5634f1
Kristen Rupinen <kristenrupinen@Kristens-MacBook-Pro-2.local>
2021-11-26 20:43:08 -0500

    moved onload to else

--- resources/scripts/registertype.js
+++ resources/scripts/registertype.js
@@ -1,18 +1,18 @@
 
 function redirectAttendee() {
     if (sessionStorage.getItem("loggedInAttendee") !== null) {
-        window.onbeforeunload.alert("Please log in before registering");
         window.location.href = "attendeeregistration.html";
     } else {
+        window.onbeforeunload.alert("Please log in before registering");
         window.location.href = "attendeelogin.html";
     }
 }
 
 function redirectVendor() {
     if (sessionStorage.getItem("loggedInVendor") !== null) {
-        window.onbeforeunload.alert("Please log in before registering");
         window.location.href = "vendorregistration.html";
     } else {
+        window.onbeforeunload.alert("Please log in before registering");
         window.location.href = "index.html";
     }
 }
