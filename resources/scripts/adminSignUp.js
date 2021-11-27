089dc68f2cfd8fa1edd74d71a1f07da782e27613
Kristen Rupinen <kristenrupinen@Kristens-MacBook-Pro-2.local>
2021-11-26 23:39:11 -0500

    su

--- resources/scripts/adminSignUp.js
+++ resources/scripts/adminSignUp.js
@@ -1,6 +1,6 @@
 function handleOnSubmit()
 {
-    const feedUrl = "https://big-als-farmers-market-backend.herokuapp.com/api/Admin";
+    const feedUrl = "https://big-als-farmers-market-backend.herokuapp.com/api/admin";
     fetch(feedUrl, {
         method: "POST",
         headers: 
