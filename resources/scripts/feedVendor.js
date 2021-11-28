3489f02021256f01a25cb1448763f478b0db483d
Kristen Rupinen <kristenrupinen@Kristens-MacBook-Pro-2.local>
2021-11-28 15:54:34 -0600

    registration button change

--- resources/scripts/feedVendor.js
+++ resources/scripts/feedVendor.js
@@ -39,6 +39,41 @@ function setFeed(vendors)
     html += `</div>`;
 });
 
+function registerVendor()
+{
+    const feedUrl = "https://big-als-farmers-market-backend.herokuapp.com/api/Vendor/vendorRegister";
+    fetch(feedUrl, {
+        method: "POST",
+        headers: 
+        {
+            "Accept": "application/json",
+            "Content-type": "application/json"
+        },
+
+        body: JSON.stringify({
+            venNum : sessionStorage.getItem("loggedInVendor")
+        })
+    }).then(response => response.json())
+    .then(function(vendor){
+        if(vendor.RegisterSpot==0)
+        {
+            window.alert("You have been registered! Your Number is " +vendor.RegisterSpot);
+        }
+        else{
+            window.alert("You have been registered! Your Number is "+vendor.RegisterSpot);
+        }
+        
+        window.location.href = "feedVendor.html";
+        console.log(json);
+    }).catch(function(error){
+        console.log(error);
+        //"login not found"
+        
+    });
+
+}
+
+
     
 //close unordered list
 html += "</ul>";
