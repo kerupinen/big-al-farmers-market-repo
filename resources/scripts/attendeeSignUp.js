b38151fbd3278e4b69dce9a5d67f6f08c71e78c2
raegan stange :) <rcstange@crimson.ua.edu>
2021-12-01 03:38:33 -0600

    ?

--- resources/scripts/attendeeSignUp.js
+++ resources/scripts/attendeeSignUp.js
@@ -1,6 +1,6 @@
 function handleOnSubmit()
 {
-    const feedUrl = "https://big-als-farmers-market-backend.herokuapp.com/api/Admin/adminPost";
+    const feedUrl = "https://big-als-farmers-market-backend.herokuapp.com/api/Attendee/attendeePost";
     if(document.getElementById("userBox").value==''||document.getElementById("passBox").value=='')
         {
             window.alert("Please enter a value into each field");
@@ -21,10 +21,10 @@ function handleOnSubmit()
         })
 
         
-        }).then(function(admin){
+        }).then(function(attendee){
         window.alert("You have been signed up!");
-        window.location.href = "adminlogin.html";
-        console.log(admin);
+        window.location.href = "attendeelogin.html";
+        console.log(attendee);
         }).catch(function(error){
         window.onerror = function()
         {
--- resources/scripts/vendorSignUp.js
+++ resources/scripts/vendorSignUp.js
@@ -1,6 +1,6 @@
 function handleOnSubmit()
 {
-    const feedUrl = "https://big-als-farmers-market-backend.herokuapp.com/api/Admin/adminPost";
+    const feedUrl = "https://big-als-farmers-market-backend.herokuapp.com/api/Vendor/vendorPost";
     if(document.getElementById("userBox").value==''||document.getElementById("passBox").value=='')
         {
             window.alert("Please enter a value into each field");
@@ -21,10 +21,10 @@ function handleOnSubmit()
         })
 
         
-        }).then(function(admin){
+        }).then(function(vendor){
         window.alert("You have been signed up!");
-        window.location.href = "adminlogin.html";
-        console.log(admin);
+        window.location.href = "vendorlogin.html";
+        console.log(vendor);
         }).catch(function(error){
         window.onerror = function()
         {
