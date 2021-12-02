4810934e9c28bed73776c00f2f921d5191fb3ae9
Kristen Rupinen <kristenrupinen@Kristens-MacBook-Pro-2.local>
2021-12-01 21:03:51 -0600

    trying edit

---
+++ resources/scripts/editVendor.js
@@ -0,0 +1,35 @@
+function handleOnSubmit()
+{
+    const feedUrl = "https://big-als-farmers-market-backend.herokuapp.com/api/Vendor/"+sessionStorage.getItem("loggedInVendor");
+    if(document.getElementById("merchBox").value==''||document.getElementById("descBox").value==''||document.getElementById("merchBox").value=='')
+        {
+            window.alert("Please enter a value into each field");
+        }
+    else
+    {
+    fetch(feedUrl, {
+        method: "PUT",
+        headers: 
+        {
+            "Accept": "application/json",
+            "Content-type": "application/json"
+        },
+
+        body: JSON.stringify({
+            merchType : document.getElementById("merchBox").value,
+            images : document.getElementById("imageBox").value,
+            desc : document.getElementById("descBox").value,
+            venName : document.getElementById("nameBox").value
+        })
+
+        
+        }).then(function(vendor){
+        window.alert("The feed had been edited!");
+        window.location.href = "feedVendor.html";
+        console.log(vendor);
+        }).catch(function(error){
+        console.log(error);
+    });
+    }
+
+}
